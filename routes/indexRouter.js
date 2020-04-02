a5c9080373e766b35ad63d81193c172d8510c487
harshit-saini <harshitr.x.t.10@gmail.com>
2020-04-03 00:09:16 +0530

    initial commit

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,77 @@
 const express = require("express");
+const axios = require("axios").default;
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.render("index");
-})
-router.get("/login", (req, res, next) => {
-    res.render("login");
-})
+let india_data;
+let allStatesData;
 
+async function getData() {
+  const config = {
+    method: "GET",
+    url: "https://corona-virus-world-and-india-data.p.rapidapi.com/api",
+    // `headers` are custom headers to be sent
+    headers: {
+      "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
+      "x-rapidapi-key": "18b229f2a8mshb23a12aa878c3cep1f90f0jsncecb17e2091a"
+    }
+  };
+
+  try {
+    let res = await axios(config);
+    // console.log(res.data.countries_stat);
+    let allCountriesData = res.data.countries_stat;
+    india_data = allCountriesData.find(
+      country => country.country_name === "India"
+    );
+    // console.log(india_data.country_name);
+    // console.log(typeof india_data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData();
+
+async function getIndiaData() {
+  const config2 = {
+    method: "GET",
+    url: "https://corona-virus-world-and-india-data.p.rapidapi.com/api_india",
+    // `headers` are custom headers to be sent
+    headers: {
+      "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
+      "x-rapidapi-key": "18b229f2a8mshb23a12aa878c3cep1f90f0jsncecb17e2091a"
+    }
+  };
+
+  try {
+    let res = await axios(config2);
+    // console.log(res.data.countries_stat);
+    allStatesData = res;
+
+    // console.log(india_data.country_name);
+    // console.log(typeof india_data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getIndiaData();
+
+router.get("/", (req, res, next) => {
+  res.render("index", {
+    data: {
+      india_data
+      // allStatesData
+    }
+  });
+});
+
+// router.get("/api", (req, res, next) => {
+//   console.log(typeof allStatesData);
+//   res.json({
+//     allStatesData: "hello"
+//   });
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const express = require("express");
-require("dotenv").config();
+// require("dotenv").config();
 const ejs = require("ejs");
 const expressEjsLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/indexRouter");
 
 const app = express();
 
-// settings to serve static files
-app.use(express.static("public"))
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// settings to serve static files
+app.use(express.static("public"));
 
 // setting for the view engine
 app.set("views", "view");
 app.set("view engine", "ejs");
 app.use(expressEjsLayouts);
 
-// 
+//
 
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`server started...on port ${PORT}`);
-})
+  console.log(`server started...on port ${PORT}`);
+});
